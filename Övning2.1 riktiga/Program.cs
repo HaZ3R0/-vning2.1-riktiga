7159a3148625dcf8ece9b77452280326e69d1504
majo06005 <majo06005@E-PW0234K2>
2023-09-08 13:29:41 +0200

    Add project files.

---
+++ Övning2.1 riktiga/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Övning_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilken stad kommer du ifrån?");
+            string stad = Console.ReadLine();
+            Console.WriteLine("vad är ditt drömresemål?");
+            string resemål = Console.ReadLine();
+
+            Console.WriteLine("Hej " + stad +"sbo. Jag hoppas att du får resa till " + resemål + ".");
+            Console.ReadKey();
+        }
+    }
+}
